7b8ab6c0a3955ffaee332030bd29a07666741965
HartejSingh2001 <86732914+HartejSingh2001@users.noreply.github.com>
2022-02-16 21:42:29 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,22 @@
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    if(top<1000){
+    // 1000 given as the size of the stack int the driver code
+        top++;
+        arr[top]=x;
+    }
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    if(top==-1){
+        return -1;
+    }      
+    else{
+        int temp=arr[top];
+        top--;
+        return temp;
+    }
+}
